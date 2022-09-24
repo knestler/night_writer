20b94b51e82aa4dd1233d3082aad533a0b43aa45
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-24 14:36:27 -0400

    fix: removed test and moved to write braille spec

--- spec/night_writer_spec.rb
+++
@@ -1,8 +0,0 @@
-require './lib/dictionary/'
-
-RSpec.describe do Dictionary
-  it 'can work' do
-    dictionary = Dictionary.new
-    expect(dictionary.translate_to_braille('kristen')).to eq('0.....')
-  end
-end
