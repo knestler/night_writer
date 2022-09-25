00aa483589fc60c79e8271a3e318a8336f0cd26a
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-25 17:35:04 -0400

    fix: line limitations and refactoring

--- lib/write_braille.rb
+++ lib/write_braille.rb
@@ -3,36 +3,20 @@ class WriteBraille
 
   def translate_to_braille(text)
     braille_phrase = []
-    text.downcase.chars.map do |char|
-      lowercase.each do |letter, braille|
-        if char == letter
-          braille_phrase << braille
-        end
-      end
-    end
-    braille_phrase
+    text.downcase.chars.map {|char| lowercase.each { |letter, braille| braille_phrase << braille if char == letter}}; braille_phrase
   end
 
   def write_braille(text)
-    row1 = []
-    row2 = []
-    row3 = []
-    
-    translate_to_braille(text).each do |line|
-      row1 << line[0]+line[1]
-      row2 << line[2]+line[3]
-      row3 << line[4]+line[5]
+    row1 = []; row2 = []; row3 = []
+    translate_to_braille(text).each { |line|row1 << line[0]+line[1]; row2 << line[2]+line[3]; row3 << line[4]+line[5]}
+    until row1[40..-1] == nil
+      p row1[0..39].join, row2[0..39].join, row3[0..39].join 
+      row1 = row1[40..-1]; row2 = row2[40..-1]; row3 = row3[40..-1]
     end
-    
-    until row1 == nil
-      print row1[0..39].join #+ "\n" + 
-      print row2[0..39].join #+ "\n" + 
-      print row3[0..39].join
-
-      row1 = row1[40..-1]
-      row2 = row2[40..-1]
-      row3 = row3[40..-1]
+    if row1[40..-1] == nil
+      p row1[0..39].join, row2[0..39].join, row3[0..39].join
     end
-    # require 'pry'; binding.pry
   end
+
+
 end
