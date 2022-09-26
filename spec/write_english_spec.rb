c3b93d8510a6dc43b40c45328e0d5cda9a303831
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-25 22:28:56 -0400

    fix: english spec fixed tests

--- spec/write_english_spec.rb
+++ spec/write_english_spec.rb
@@ -3,12 +3,11 @@ require 'spec_helper'
 RSpec.describe do WriteEnglish
   it 'can translate to braille' do
     braille = WriteEnglish.new
-    expect(braille.translate_to_english('0...0.')).to eq('k')
-    # expect(phrase.translate_to_braille('kristen')).to eq(["0...0.", "0.000.", ".00...", ".00.0.", ".0000.", "0..0..", "00.00."])
+    expect(braille.translate_to_english("0.0..0.0.00.00\n..000.0.00.0.0\n0.0...0.0...0.")).to eq('kristen')
   end
 
-  xit 'can print out brail to terminal' do 
-    phrase = WriteEnglish.new
-    expect(braille.write_english(["0.0..0.0.00.00", "..000.0.00.0.0", "0.0...0.0...0."])).to eq(['kristen'])
+  it 'can print out braille to terminal' do 
+    braille = WriteEnglish.new
+    expect(braille.write_english("0.0.\n..00\n0.0.")).to eq([["0.", "..", "0."], ["0.", "00", "0."]])
   end
 end
