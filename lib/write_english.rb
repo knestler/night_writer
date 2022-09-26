81a812eb6e5566b88fae82735a3180bffb01d13a
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-25 22:49:12 -0400

    fix: write english method

--- lib/write_english.rb
+++ lib/write_english.rb
@@ -3,13 +3,18 @@ class WriteEnglish
 
   def translate_to_english(text)
     english_phrase = []
-    letters = text.chars.each_slice(6).map(&:join)
-    letters.each do |element|
+    write_english(text).each do |element|
       lowercase.each do |letter, braille| 
-        english_phrase << letter if element == braille
+        english_phrase << letter if element.join == braille
       end
     end
     english_phrase.join
   end
 
+  def write_english(text)
+    x = text.split("\n").map do |row|
+      row.scan(/../)
+    end
+    x = x.transpose
+  end
 end
