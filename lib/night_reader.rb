1b489eccbb1c3116e122ea359d41f8a32f9841ca
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-27 15:20:19 -0400

    fix: file parser read test

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,15 +1,4 @@
 require './spec/spec_helper/'
 
-reader = File.open(ARGV[0], 'r')
-incoming_text = reader.read
-reader.close
-
-english= WriteEnglish.new.translate_to_english(incoming_text)
-
-writer = File.open(ARGV[1], 'w')
-writer.write(english)
-
-text_count =  "Created #{ARGV[1]} containing #{incoming_text.length} characters"
-p text_count
-
-writer.close
+file_parser = FileParser.new
+file_parser.start
